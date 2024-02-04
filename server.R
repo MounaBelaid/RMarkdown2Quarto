9137e09a9b04d71e028d6c92b0d90fb287b914f4
MounaBelaid <belaid.mounaa@gmail.com>
2024-02-04 01:18:00 +0100

    internal: add new features

---
+++ server.R
@@ -0,0 +1,150 @@
+suppressPackageStartupMessages({
+  library(shiny)
+  library(readr)
+  library(stringr)
+  library(dplyr)
+  library(zip)
+  library(knitr)
+  library(tools)
+  library(fs)
+  library(shinyFiles)
+  library(shinyAce)
+})
+
+# appends a numeric suffix to non-unique selected file names
+addNumericSuffix <- function(vec) {
+  counts <- table(vec)
+  duplicates <- names(counts[counts > 1])
+
+  for (dup in duplicates) {
+    indices <- which(vec == dup)
+    if (length(indices) > 1) {
+      for (i in seq_along(indices)[-1]) {
+        vec[indices[i]] <- paste0(dup, i)
+      }
+    }
+  }
+
+  return(vec)
+}
+
+shinyServer(function(input, output, session) {
+
+  updated_content <- reactiveVal(NULL)
+  rmd_content <- read_lines("demo_file.Rmd")
+  rmd_content <- paste(rmd_content, collapse = "\n")
+  updateAceEditor(session, "demo_file_content", value = rmd_content)
+  observeEvent(input$demo_file_content, {
+    updated_content(input$demo_file_content)
+  })
+
+  output$exportButton <- downloadHandler(
+    filename = function() {
+      paste0("demo_file", ".Rmd")
+    },
+    content = function(file) {
+      write_lines(updated_content(), file)
+    }
+  )
+
+  volumes <- c(Home = path_home(), "R Installation" = R.home(), getVolumes()())
+  shinyDirChoose(input, "directory", roots = volumes, session = session, restrictions = system.file(package = "base"), allowDirCreate = FALSE)
+
+  files_info <- reactive({
+    rmd_files <- input$rmdfiles
+    req(rmd_files)
+    process_rmd_files <- function(i){
+      extension <- file_ext(rmd_files$datapath[i])
+      icon <- ifelse(extension == "Rmd", as.character(icon("check", class = "checkclass")),
+                     as.character(icon("xmark", class = "xmarkclass")))
+      dat <- data.frame(
+        Filename = rmd_files$name[i],
+        File.format.validity = icon
+      )
+
+      return(dat)
+    }
+    df_imported_files <- lapply(seq_along(rmd_files$datapath), process_rmd_files)
+    file_data <- bind_rows(df_imported_files)
+
+    return(file_data)
+  })
+
+  files_info_from_folder <- reactive({
+    req(input$directory)
+    files_in_selected_folder <- list.files(parseDirPath(volumes, input$directory), pattern = "\\.Rmd$")
+    if (length(files_in_selected_folder) > 0) {
+      dat_folder <- data.frame(
+        Filename = files_in_selected_folder,
+        File.format.validity = as.character(icon("check", class = "checkclass")))
+    }
+    else {
+      # Handle the case when there are no files in the folder
+      dat_folder <- data.frame(Filename = character(0), File.format.validity = character(0))
+    }
+
+    return(dat_folder)
+  })
+
+  output$fileTable <- renderTable(
+    files_info(),
+    sanitize.text.function = function(x) x
+  )
+
+  output$fileTableFromFolder <- renderTable(
+    files_info_from_folder(),
+    sanitize.text.function = function(x) x
+  )
+
+  output$downloadButtonFiles <- downloadHandler(
+        filename = function() {
+          paste0("Quarto_converted_files_", format(Sys.time(), "%Y%m%d"), "_",  format(Sys.time(), "%H%M%S"),".zip")
+        },
+        content = function(fname) {
+          files_path <- c()
+          parse_dir <- parseDirPath(volumes, input$directory)
+          rmd_files <- input$rmdfiles
+          rmd_files <- rmd_files[file_ext(rmd_files$datapath) == "Rmd", , drop = FALSE]
+          files_in_folder <- list.files(parse_dir, pattern = "\\.Rmd$", full.names = TRUE)
+
+          if (length(files_in_folder) > 0 & length(rmd_files) > 0) {
+            rmd_paths <- c(rmd_files$datapath, files_in_folder)
+            files_in_folder <- list.files(parse_dir, pattern = "\\.Rmd$")
+            rmd_names <- c(file_path_sans_ext(rmd_files$name), file_path_sans_ext(files_in_folder))
+            rmd_names <- addNumericSuffix(rmd_names)
+
+          }
+          else if(length(files_in_folder) > 0) {
+            files_in_folder <- list.files(parse_dir, pattern = "\\.Rmd$", full.names = TRUE)
+            rmd_paths <- files_in_folder
+            files_in_folder <- list.files(parse_dir, pattern = "\\.Rmd$")
+            rmd_names <- file_path_sans_ext(files_in_folder)
+            rmd_names <- addNumericSuffix(rmd_names)
+          }
+          else if(length(rmd_files) > 0) {
+            rmd_paths <- rmd_files$datapath
+            rmd_names <- file_path_sans_ext(rmd_files$name)
+            rmd_names <- addNumericSuffix(rmd_names)
+          }
+
+          for (i in seq_along(rmd_paths)) {
+            content <- read_lines(rmd_paths[i])
+            if ("output: pdf_document" %in% content) {
+              content <- str_replace_all(content, pattern = "output: pdf_document", replacement = "format: pdf")
+            } else if ("output: word_document" %in% content) {
+              content <- str_replace_all(content, pattern = "output: word_document", replacement = "format: word")
+            }
+            else if ("output: html_document" %in% content) {
+              content <- str_replace_all(content, pattern = "output: html_document", replacement = "format: html")
+            }
+            qmd_path <- paste0(rmd_names[i], ".qmd")
+            write_lines(x = content, file = qmd_path)
+            convert_chunk_header(qmd_path, output = qmd_path)
+            files_path <- c(files_path, qmd_path)
+          }
+
+          zip(zipfile = fname, files = files_path)
+        },
+        contentType = "application/zip"
+      )
+})
---
+++ ui.R
@@ -0,0 +1,65 @@
+suppressPackageStartupMessages({
+  library(shiny)
+  library(bslib)
+  library(shinyFiles)
+  library(shinyAce)
+})
+
+shinyUI(fluidPage(
+  theme = bslib::bs_theme(
+    version = 5,
+    bootswatch = "flatly",
+    primary = "#2b8cbe",
+    lightness = 40
+  ),
+  tags$img(src = "rmd_quarto.png", style = "width: 50%; height: auto;"),
+  tags$style(".checkclass {color:#31a354}"),
+  tags$style(".xmarkclass {color:#f03b20}"),
+  titlePanel("Convert your Rmd files to Quarto"),
+  sidebarLayout(
+    sidebarPanel(
+      downloadButton("exportButton", "Export the demo file"),
+      br(), br(),
+      aceEditor("demo_file_content", value = "", mode = "markdown", height = "250px"),
+      br(),
+      fileInput("rmdfiles", "Choose your Rmd Files", accept = ".Rmd", multiple = TRUE),
+      tableOutput("fileTable"),
+      br(),
+      h6(icon("circle-exclamation", style = "color: red"),
+              "Take advantage of processing all the Rmd files in a selected folder only when the app is running locally."),
+      shinyDirButton("directory", "Select a folder", "Please select a folder"),
+      tableOutput("fileTableFromFolder"),
+      br(),
+      div(
+        style = "text-align:left; font-size: 15px",
+        strong("View Code "), a(icon("fab fa-github"), href = "https://github.com/MounaBelaid/RMarkdown2Quarto", target = "_blank")
+      ),
+    ),
+    mainPanel(
+      fluidPage(
+        column(
+          5,
+          downloadButton("downloadButtonFiles", "Download the Converted Quarto Files"),
+          br(), br(),
+          HTML("<p>The conversion process, which occurs behind the scenes, consists of the following steps:</p>
+               <ol>
+               <li>Locate a list of available RMarkdown files for conversion to Quarto. If you run the application
+               locally, you can choose a folder from which only .Rmd files will be selected. If you don't have an Rmd file on hand,
+               you can export the demo RMarkdown file and import it to test the application. </li>
+               <li>Convert each selected file from the .Rmd format to the .qmd format and
+               display the selected .Rmd files in tables.</li>
+               <li>Transform the Rmd syntax format to the qmd format in the front matter. The available format syntaxes
+               to change are :
+              <ul>
+                <li>output: pdf_document</li>
+                <li>output: word_document</li>
+                <li>output: html_document</li>
+              </ul> </li>
+               <li>Convert the in-header chunk option syntax to the in-body syntax in
+                the converted Quarto files.</li>
+               </ol>")
+        )
+      )
+    )
+  )
+))
